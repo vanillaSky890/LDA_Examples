db30a95744d402d961f713fe41e62fcfcac2ff3a
vanillaSky890 <vanillaSky890@protonmail.com>
2024-08-15 00:51:40 +0300

    add new styles

---
+++ test1111/farmer_mentor.php
@@ -0,0 +1,437 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Kissan Mitra Portal</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="images/img.ico" type="image/x-icon" />
+
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/bootstrap-responsive.css" type="text/css" media="screen">    
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/camera.css" type="text/css" media="screen">
+
+
+<script type="text/javascript" src="js/jquery.js"></script>  
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/superfish.js"></script>
+
+<script type="text/javascript" src="js/jquery.ui.totop.js"></script>
+<script type="text/javascript" src="js/camera.js"></script>
+<script type="text/javascript" src="js/jquery.mobile.customized.min.js"></script>
+<script>
+$(document).ready(function() {
+	/////// icons
+	$(".social li").find("a").css({opacity:0.6});
+	$(".social li a").hover(function() {
+		$(this).stop().animate({opacity:1 }, 400, 'easeOutExpo');		    
+	},function(){
+	    $(this).stop().animate({opacity:0.6 }, 400, 'easeOutExpo' );		   
+	}); 
+
+	// camera
+	$('#camera_wrap').camera({
+		//thumbnails: true
+		autoAdvance			: true,		
+		mobileAutoAdvance	: true,
+		height: '37%',
+		hover: false,
+		loader: 'none',
+		navigation: false,
+		navigationHover: false,
+		mobileNavHover: false,
+		playPause: false,
+		pauseOnClick: false,
+		pagination			: true,
+		time: 7000,
+		transPeriod: 1000,
+		minHeight: '300px'
+	});
+
+	
+
+}); //
+$(window).load(function() {
+	//
+
+}); //
+</script>		
+<!--[if lt IE 8]>
+		<div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+	<![endif]-->    
+
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>      
+  <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+<![endif]-->
+</head>
+<body>
+<div id="main">
+
+<header>
+<div class="logo_wrapper"><a href="index.php" class="logo" ><img src="images/kisaan_mitra_logo_white.png" class="img-responsive" width="100px"></a></div>	
+<div class="top1">
+<div class="container">
+<div class="row">
+<div class="span12">
+	<div class="top1_inner clearfix">
+		<div class="phone1">Need Farmer Help? Call us: <span>1800 180 1551</span></div>
+		<div class="phone2 clearfix">
+			<div class="txt1">Follow us:</div>
+			<div class="social_wrapper">
+				<ul class="social clearfix">
+				    <li><a href="#"><img src="images/social_ic1.png"></a></li>
+				    <li><a href="#"><img src="images/social_ic2.png"></a></li>
+				    <li><a href="#"><img src="images/social_ic3.png"></a></li>			    
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>	
+</div>	
+</div>
+</div>	
+<div class="top2">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="top2_inner">
+<div class="navbar navbar_">
+	<div class="navbar-inner navbar-inner_">
+		<a class="btn btn-navbar btn-navbar_" data-toggle="collapse" data-target=".nav-collapse_">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</a>
+		<div class="nav-collapse nav-collapse_ collapse">
+			<ul class="nav sf-menu clearfix">
+				<li class="active"><a href="index.php">Home</a></li>				
+				<li><a href="about.php">About Us</a></li>				
+				<li class="sub-menu sub-menu-1"><a href="program.php">Programme & Schemes<em></em></a>
+					<ul>
+						<li><a href="#">Year Wise</a></li>
+						<li class="sub-menu sub-menu-2"><a href="#">History<em></em></a>
+							
+						</li>
+						
+						<li><a href="#">Media</a></li>						
+					</ul>						
+				</li>
+				<li class="sub-menu sub-menu-1"><a href="login.php">Login<em></em></a>
+					<ul>
+						<li><a href="#">Farmer Login</a></li>						
+						<li><a href="#">Mentor Login</a></li>
+						<li><a href="#">Seller Login </a></li>						
+						<li><a href="#">Buyer Login</a></li>						
+					</ul>						
+				</li>
+													
+				<li><a href="contact.php">Contacts</a></li>											
+			</ul>
+		</div>
+	</div>
+</div>
+</div>
+</div>	
+</div>	
+</div>
+</div>
+</header>	
+<div id="inner2">
+	<div class="container">
+		<div class="row">
+			<div class="span8">
+				
+				<h1 class="home">Welcome!<br><span>Beginning Farmer Mentorship Program</span></h1>
+
+				<h4>
+				    <u>	Mentorship Program</u><br><br>
+
+Application is available for download at the bottom of this page.<br><br>
+
+
+The Beginning Farmer Program of Kissan Mitra is excited to present the 2018 Mentorship Program.  Each year  10 beginning farmers with experienced growers who can help them work towards specific learning goals.  This program is geared towards beginning farmers who have enough experience to move into farm management/ownership roles and can benefit from the support and input of a seasoned grower.  Participants work with their mentors to make specific learning plans which are supplimented by group meetings, conferences and workshops lead by Kissan mitra.<br><br>
+
+
+The KIssan mitra  Beginning Farmer Mentorship Program aims to help fill the education gap between apprentice/farm worker and independent farmer. By pairing mentees with experienced growers who are not their employers, the program enables aspiring farmers to gain farming experiences, skills, and perspectives that they may otherwise miss in their day-to-day work.
+
+
+				</h4>
+				<p>
+					<b><u>PROGRAM BENEFITS</u></b><br><br>
+
+
+Mentor Pairing: Mentees will be paired with mentors based on interest, goals and location.  Applicants can request or suggest mentors they are interested in working with or NOFA/Mass will find a strong mentor match.<br><br>
+
+<b><u>Workshops and Conferences:</u></b> Acceptance to the program will give mentees access to the Winter Conference, Summer Conference and stipends to Advanced Grower and on-farm workshops hosted by NOFA/Mass in 2017.   Program participants are required to attend a program kick-off meeting at the Winter Conference and are expected to attend the mid-season program meeting at the Summer Conference.<br><br>
+
+
+<b><u>Mentorship Structure:</u></b> At the Winter Conference, the Beginning Farmer Program Coordinator will facilitate goal setting and the structuring of the mentorship based on the individuals in each mentor relationship.  Mentees and mentors can choose communication strategies, set dates to visit each other’s farms and map out a plan for the year.<br><br>
+
+
+<b><u>Support:</u></b> Mentees have access to the Beginning Farmer Program Coordinator who can help facilitate the development of learning plans and connect mentees to resources that aid in their professional development as farmers.  Mentees will also connect with other participants at the conferences to develop a network of beginning farmers with whom they can share information and resources.<br><br>
+
+
+<b><u>PROGRAM DETAILS</u></b>
+
+<b><u>Program Timeline:</u></b> January-December 2018<br><br>
+
+
+<b><u>Application Deadline:</u></b> December 7, 2018<br><br>
+
+
+<b><u>Major Events:</u></b> Winter Conference Kick-off meeting, January 14th, 2017 and Summer Conference mid-season meeting, August 2017.<br><br>
+
+
+<b><u>Quarterly Reporting/Check-In:</u></b> Communicate with Beginning Farmer Program Coordinator once per season to update on the progress of the mentorship.  Complete bi-annual evaluations to update NOFA/Mass on the progress of your mentorship.<br><br>
+
+
+<b><u>Program Cost:</u></b> $200, NOFA/Mass Membership required. To become a NOFA/Mass member and see all the benefits that membership offers, visit www.nofamass.org/membership. <br><br>
+
+
+<b><u>WHO SHOULD APPLY</u></b>
+
+ “A mentee who has specific learning goals and/or is someone who wants to explore another perspective into their individual farming interests.” 
+- Recommendation from previous participant
+
+Beginning Farmers who have some farming experience and are interested in deepening their knowledge or pursuing specific goals related to their agricultural career should apply.  Applicants do not need to have access to land or an established farm business to apply. 
+
+ We are seeking participants dedicated to a year of professional development. <br><br>
+
+
+ <b><u>Example Goals of Previous Years Mentees</u></b>
+
+            • Develop a season extension plan for their farm
+            • Deepen understanding of soil fertility management
+            • Improved seedling production and greenhouse management
+            • Raise pigs in conjunction with a vegetable operation
+            • Develop a business plan for a new farm
+            • General management assistance for new managers
+            • Learn more about employee management, labor structures
+            • Explore the farm model and philosophy of their mentor farmer <br><br>
+
+
+
+
+<b><u>INFORMATION FOR POTENTIAL MENTORS</u></b>
+
+The decision to become a mentor should be made carefully with the understanding that as a mentor you will be expected to share some of your time and knowledge with a developing farmer. Our hope is that the process and relationship will prove rewarding for both the mentee and the mentor.
+
+“[Being a Mentor] makes you work harder to make your own farm beautiful and productive to show off to your mentee! Also, you get to compare and contrast how things are growing on different sites based on different conditions. Finally, [being a mentor] helps build a community of people interested in local food production.”
+– Past Mentor
+
+EXPECTATIONS <br><br>
+
+
+NOFA/Mass expects mentors to: <br><br>
+
+Make a 12-month commitment to be a mentor.
+Give approximately 15 hours over the course of the 12-month program to the mentee with whom they are paired. (More contact is encouraged, if appropriate.)
+Share advice, technical assistance, wisdom, philosophies and sympathy with diligence and honesty, to the best of the mentor's ability, via phone, email and face-to-face meetings.
+Host the mentee at your farm at least once during the course of the program.
+Attend the program kick-off meeting at the NOFA/Mass Winter Conference (January 16, 2016 – Worcester) with all other mentor and mentee participants.
+<br><br>
+<b><u>BENEFITS</u></b>
+
+In recognition of the important service that the mentor provides, NOFA/Mass agrees to provide each mentor an honorarium of $200 and a $35 discount on the registration fee for the Winter Conference. 
+<br><br>
+In coordination with other NOFA chapters, NOFA/Mass will also invite mentors to an annual get-together with other mentors from around the region. This will be an opportunity for veteran farmers who are committed to educating the next generation of growers to meet and share knowledge, experiences, and support.
+<br><br>
+MENTOR EXPERIENCE
+
+If you are interested in mentoring a beginning farmer and would consider being paired by our mentorship program, please contact Beginning Farmer Program Coordinator, Brittany Sidway Overshiner.
+				</p>
+				<a href="#" class="button1">Read more about us</a>
+
+				<div class="line1"></div>
+
+				<h2 class="home">New generation of farming</h2>
+
+				<div class="thumb1">
+					<div class="thumbnail clearfix">
+						<figure class="img-polaroid"><img src="images/home01.jpg" alt=""></figure>
+						<div class="caption">
+							<h4>
+								Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Ipsum dolor sit.
+							</h4>
+							<p>
+								amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat consectetuer adipiscing elit.
+							</p>
+							<a href="#" class="button1">Details</a>
+						</div>
+					</div>
+				</div>
+				<div class="thumb1">
+					<div class="thumbnail clearfix">
+						<figure class="img-polaroid"><img src="images/home02.jpg" alt=""></figure>
+						<div class="caption">
+							<h4>
+								Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Ipsum dolor sit.
+							</h4>
+							<p>
+								amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat consectetuer adipiscing elit.
+							</p>
+							<a href="#" class="button1">Details</a>
+						</div>
+					</div>
+				</div>
+
+
+
+
+			</div>
+			<div class="span4">
+				
+				<h3 class="home">NEWS<br><span>Latest news and events</span></h3>
+
+				<div class="thumb2"><marquee direction = "up">
+
+					<div class="thumbnail clearfix">
+						<figure class=""><img src="images/home03.jpg" alt=""></figure>
+						<div class="caption">
+							<h4>
+								<a href="#">OVER ME
+मुख्यमंत्राी श्रीमती वसुन्धरा राजे को निगम अध्यक्ष श्री जनार्दन सिंह गहलोत ने चैक भेंट किया । श्रीमती राजे ने निगम की बेहतर व्यवस्थाओं, कार्यशैली तथा लाभ में बढ़ोतरी के लिए श्री गहलोत के प्रयासों को सराहा।</a>
+							</h4>
+							<p>
+								Amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <a href="#">...</a>
+							</p>							
+						</div>
+					</div>
+				</div>
+				<div class="thumb2">
+					<div class="thumbnail clearfix">
+						<figure class=""><img src="images/home04.jpg" alt=""></figure>
+						<div class="caption">
+							<h4>
+								<a href="#">Maecenas sit</a>
+							</h4>
+							<p>
+								Amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <a href="#">...</a>
+							</p>							
+						</div>
+					</div>
+				</div>
+				<div class="thumb2">
+					<div class="thumbnail clearfix">
+						<figure class=""><img src="images/home05.jpg" alt=""></figure>
+						<div class="caption">
+							<h4>
+								<a href="#">Vestibulum vel</a>
+							</h4>
+							<p>
+								Amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <a href="#">...</a>
+							</p>							
+						</div>
+					</div>
+				</div>
+				</marquee>
+				<div class="text-right"><a href="#" class="button1">More news</a></div>
+
+				<div class="line1"></div>
+
+				<h3>What we offer</h3>
+
+				<div class="accordion" id="accordion1">
+					<div class="accordion-group">
+					    <div class="accordion-heading">
+					        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapse1">
+					        	Sell & Buy Crops
+					        </a>
+					    </div>
+					    <div id="collapse1" class="accordion-body collapse in">
+					        <div class="accordion-inner">
+					        	<h4></h4>
+					        	The farmer will register with the help of e-Mitra at the Kisan Mitra Portal, and to provide telephonic support to the portal, he will cooperate through the voice call for the uneducated farmers, sell the crop directly and buyer's registration with the help of this portal and buy the direct  crop from those farmers. So the farmer will get good money.
+					        </div>
+					    </div>
+					</div>
+					<div class="accordion-group">
+					    <div class="accordion-heading">
+					        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapse2">
+					        	Farmer Mentorship program.
+					        </a>
+					    </div>
+					    <div id="collapse2" class="accordion-body collapse">
+					        <div class="accordion-inner">
+					        	<h4>Benifit</h4>
+					        	THE KISSAN MITRA BEGINNING FARMER MENTORSHIP PROGRAM AIMS TO HELP FILL THE EDUCATION GAP BETWEEN APPRENTICE/FARM WORKER AND INDEPENDENT FARMER. BY PAIRING MENTEES WITH EXPERIENCED GROWERS WHO ARE NOT THEIR EMPLOYERS, THE PROGRAM ENABLES ASPIRING FARMERS TO GAIN FARMING EXPERIENCES, SKILLS, AND PERSPECTIVES THAT THEY MAY OTHERWISE MISS IN THEIR DAY-TO-DAY WORK.
+					        </div>
+					    </div>
+					</div>
+					<div class="accordion-group">
+					    <div class="accordion-heading">
+					        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1" href="#collapse3">
+					        	Knowledge Base
+					        </a>
+					    </div>
+					    <div id="collapse3" class="accordion-body collapse">
+					        <div class="accordion-inner">
+					        	<h4>Farmer Facility</h4>
+					        	Knowledge base segment will provides all about the knowledge of farming
+					        </div>
+					    </div>
+					</div>					
+				</div>
+
+
+
+
+
+			</div>
+		</div>
+	</div>
+</div>	
+</div>
+	
+</div>	
+</div>
+
+<footer>
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="bot clearfix">
+<div class="bot1">
+<div class="menu_bot">
+    <ul id="menu_bot" class="clearfix">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About us</a></li>
+      <li><a href="program.php">Programe & Schems</a></li>
+      <li><a href="index-3.html">Login</a></li>
+      
+      <li><a href="index-5.html">Contacts</a></li>          
+    </ul>
+</div>
+</div>	
+<div class="bot2 clearfix">
+<div class="search-form-wrapper clearfix">
+	<form id="search-form" action="search.php" method="GET" accept-charset="utf-8" class="navbar-form" >
+		<input type="text" name="s" value='Search' onBlur="if(this.value=='') this.value='Search'" onFocus="if(this.value =='Search' ) this.value=''">
+		<a href="#" onClick="document.getElementById('search-form').submit()"></a>
+	</form>
+</div>
+</div>
+</div>
+<div class="copyright">Copyright © 2013. All rights reserved.</div>
+</div>	
+</div>	
+</div>		
+</footer>
+
+
+
+
+
+
+
+
+	
+</div>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
